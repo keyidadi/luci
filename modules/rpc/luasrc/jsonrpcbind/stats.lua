c0639f6472c3a9d91813b48a998d7197af72e991
Asura Liu <keyidadi@gmail.com>
2015-05-17 23:24:51 +0800

    Add method mode in stats, that return the router's working mode.

--- modules/rpc/luasrc/jsonrpcbind/stats.lua
+++ modules/rpc/luasrc/jsonrpcbind/stats.lua
@@ -13,15 +13,129 @@ http://www.apache.org/licenses/LICENSE-2.0
 $Id$
 ]]--
 
+local ipairs,pairs,type,print,table = ipairs,pairs,type,print,table
+local require = require
+
 local sys   = require "luci.sys"
 
 module "luci.jsonrpcbind.stats"
 _M, _PACKAGE, _NAME = nil, nil, nil
 
 function getiwlist(...)
-	return sys.wifi.getiwinfo_item(..., "assoclist")
+    return sys.wifi.getiwinfo_item(..., "assoclist")
 end
 
 arp = sys.net.arptable
 
-sysinfo = sys.sysinfo
+-- sysinfo = sys.sysinfo
+
+function mode()
+    local ntm = require "luci.model.network".init()
+    local fwm = require "luci.model.firewall".init()
+    local ret = {}
+
+    local wan = {}
+    local lan = {}
+    local mflag = {0,0,0,0} -- wwan 1, wlan 2, ewan 4, elan 8
+
+    -- read wan and lan from firewall zone 
+    for _, zone in ipairs(fwm:get_zones()) do
+        if zone:name() == "wan" then
+            for _, net in ipairs(zone:get_networks()) do
+                local proto = ntm:get_network(net)
+                if proto and proto:is_bridge() then
+                    for _,ifc in ipairs(proto:get_interfaces()) do
+                        wan[#wan+1] = ifc
+                        if ifc:type() == "ethernet" then
+                            mflag[3] = 1
+                        elseif ifc:type() == "wifi" then
+                            mflag[1] = 1
+                        end
+                    end
+                elseif proto then
+                    wan[#wan+1] = proto:get_interface()
+                    if proto:get_interface():type() == "ethernet" then
+                        mflag[3] = 1
+                    elseif proto:get_interface():type() == "wifi" then
+                        mflag[1] = 1
+                    end
+                end
+            end
+        end
+    end
+
+    local proto = ntm:get_network("lan")
+    if proto:is_bridge() then
+        for _,ifc in pairs(proto:get_interfaces()) do
+            lan[#lan+1] = ifc
+            if ifc:type() == "ethernet" then
+                mflag[4] = 1
+            elseif ifc:type() == "wifi" then
+                mflag[2] = 1
+            end
+        end
+    else
+        lan[#lan+1] = proto:get_interface()
+        if ifc:type() == "ethernet" then
+            mflag[4] = 1
+        elseif ifc:type() == "wifi" then
+            mflag[2] = 1
+        end
+    end
+
+    local mode_tab = {    
+                    "none",                 -- 0
+                    "wwan-only",            -- 1
+                    "repeater",             -- 2
+                    "bridge",               -- 3
+                    "wan-only",             -- 4
+                    "mix",                  -- 5
+                    "router",               -- 6
+                    "mix",                  -- 7
+                    "lan-only",             -- 8
+                    "client",               -- 9
+                    "ap"                    -- 10
+                }
+
+    if ret["mode"] == nil then
+        local flag = mflag[1]+mflag[2]*2+mflag[3]*4+mflag[4]*8
+        if flag > 10 then 
+            ret["mode"] = "mix"
+        elseif mode_tab[flag] then
+            ret["mode"] = mode_tab[flag+1]
+        end
+    end
+    return ret
+end
+
+function print_r(data, depth)
+    local depth = depth or 3
+
+    function tableprint(t, local_depth)
+        local all = {}
+
+        local function table_len(t) 
+            local i = 0 
+            for k, v in pairs(t) do 
+                i = i + 1 
+            end 
+            return i 
+        end 
+
+        for k,v in pairs(t) do
+            if k ~= "__index" then
+                if type(v) ~= "table" then
+                    all[k] = v
+                elseif table_len(v) == 0 then
+                    all[k] = ""
+                elseif local_depth < depth then
+                    all[k] = tableprint(v,local_depth+1)
+                else
+                    all[k] = "*"
+                end
+            end
+        end
+        return all
+    end
+    return tableprint(data,0)
+end
